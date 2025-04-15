627fa7326ba9c1e70a810e51d707a704bc9ddd50
Leejiwoo <easywoo980902@gmail.com>
2025-04-15 12:51:37 +0900

    improve query, later~

--- 99-improve-query/articles/views.py
+++ 99-improve-query/articles/views.py
@@ -5,8 +5,9 @@ from django.db.models import Count
 
 # Create your views here.
 def index_1(request):
-    articles = Article.objects.order_by('-pk')
-    # articles = Article.objects.annotate(Count('comment')).order_by('-pk')
+    # articles = Article.objects.order_by('-pk')
+    articles = Article.objects.annotate(Count('comment')).order_by('-pk')
+    # annotate>  1:N 관계에서 쓰는 메서드 
     context = {
         'articles': articles,
     }
@@ -14,8 +15,11 @@ def index_1(request):
 
 
 def index_2(request):
-    articles = Article.objects.order_by('-pk')
-    # articles = Article.objects.select_related('user').order_by('-pk')
+    # articles = Article.objects.order_by('-pk')
+    articles = Article.objects.select_related('user').order_by('-pk')
+    # 연결된 모델의 데이터를 한번에 가져옴
+    # 외래키 연결시
+    # 추가 검색 안하게 해서 개선
     context = {
         'articles': articles,
     }
@@ -23,8 +27,9 @@ def index_2(request):
 
 
 def index_3(request):
-    articles = Article.objects.order_by('-pk')
-    # articles = Article.objects.prefetch_related('comment_set').order_by('-pk')
+    # articles = Article.objects.order_by('-pk')
+    articles = Article.objects.prefetch_related('comment_set').order_by('-pk')
+    # 역참조할 때 사용, 일대일, 다대다
     context = {
         'articles': articles,
     }
@@ -35,11 +40,11 @@ from django.db.models import Prefetch
 
 
 def index_4(request):
-    articles = Article.objects.order_by('-pk')
-    # articles = Article.objects.prefetch_related('comment_set').order_by('-pk')
-    # articles = Article.objects.prefetch_related(
-    #     Prefetch('comment_set', queryset=Comment.objects.select_related('user'))
-    # ).order_by('-pk')
+    # articles = Article.objects.order_by('-pk')
+    articles = Article.objects.prefetch_related('comment_set').order_by('-pk')
+    articles = Article.objects.prefetch_related(
+        Prefetch('comment_set', queryset=Comment.objects.select_related('user'))
+    ).order_by('-pk')
 
     context = {
         'articles': articles,
