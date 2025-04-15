25ddf1eca90fa602dc4fc0c272a807c1c03e3a8b
Leejiwoo <easywoo980902@gmail.com>
2025-04-15 10:45:25 +0900

    add like=article/user M:N model fields

--- 03-01-many-to-many-relationships/articles/models.py
+++ 03-01-many-to-many-relationships/articles/models.py
@@ -5,6 +5,12 @@ from django.conf import settings
 # Create your models here.
 class Article(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # '좋아요' 기능은 게시글에 달려서 
+    # 매니투매니필드는 게시글모델에 작성
+    # 단순 복수형 이름보다 
+    # 만들려는 기능이 무엇인지 생각하고 명시적인 매니저 이름을 설정하기
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_articles")
+
     title = models.CharField(max_length=10)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
